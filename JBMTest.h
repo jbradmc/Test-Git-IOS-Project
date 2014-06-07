73bf694a46f9dfde240750752649afc9f77d59ea
Brad McAnalley <user@Brads-MacBook-Pro-2.local>
2014-06-07 11:06:02 -0500

    added a test class

---
+++ JBMTest.h
@@ -0,0 +1,13 @@
+//
+//  JBMTest.h
+//  Git Test
+//
+//  Created by Brad McAnalley on 6/7/14.
+//  Copyright (c) 2014 Brad McAnalley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JBMTest : NSObject
+
+@end
---
+++ JBMTest.m
@@ -0,0 +1,13 @@
+//
+//  JBMTest.m
+//  Git Test
+//
+//  Created by Brad McAnalley on 6/7/14.
+//  Copyright (c) 2014 Brad McAnalley. All rights reserved.
+//
+
+#import "JBMTest.h"
+
+@implementation JBMTest
+
+@end
